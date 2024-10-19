3e00f17d546c19cda2e4d697d0d6036965a0a464
mohitsinghnegi10 <mohitsingh10799@gmail.com>
2024-10-19 18:06:14 +0530

    add in some file

--- src/main/java/com/Sale/api/Security/SecurityConfig.java
+++ src/main/java/com/Sale/api/Security/SecurityConfig.java
@@ -38,8 +38,6 @@ public class SecurityConfig {
                         .permitAll())
                 .logout(logout -> logout
                         .permitAll());
-        
-
         return http.build();
     }
 
